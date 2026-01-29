2889ad7b82a38b17073d4cf2ccc575a90c99ed56
yuping-creator <yuping@whaletech.ai>
2026-01-29 15:16:05 +0800

    Update XS2_noise_w_disploss.sh

--- XS2_noise_w_disploss.sh
+++ XS2_noise_w_disploss.sh
@@ -13,6 +13,7 @@ python train.py \
   --epochs 80 \
   --prediction noise \
   --global-batch-size 32 \
+  --loss-weight likelihood \
   --global-seed 0 \
   --vae ema \
   --num-workers 4 \
@@ -22,3 +23,4 @@ python train.py \
   --cfg-scale 4.0 \
   --wandb \
   --disp
+
