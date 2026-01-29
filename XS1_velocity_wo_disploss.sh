b3c88402af9b996d1cac6c69fb77053aa05d05cc
yuping-creator <yuping@whaletech.ai>
2026-01-29 14:57:27 +0800

    Experimental setup

---
+++ XS1_velocity_wo_disploss.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export ENTITY=22307100070-fdsmsyp
+export PROJECT=Disperse-Loss
+export WANDB_KEY=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
+
+
+python train.py \
+  --data-path data/cifar10 \
+  --model SiT-XS/1 \
+  --image-size 32 \
+  --num-classes 10 \
+  --epochs 80 \
+  --prediction velocity \
+  --global-batch-size 32 \
+  --global-seed 0 \
+  --vae ema \
+  --num-workers 4 \
+  --log-every 100 \
+  --ckpt-every 5000 \
+  --sample-every 5000 \
+  --cfg-scale 4.0 \
+  --wandb
